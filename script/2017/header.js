d91a727fd8b017693adecdbbf87a217d6e3106c7
Dave Shea <dave@mezzoblue.com>
2017-03-07 23:13:44 -0800

    Updates for 2017

--- script/2017/header.js
+++ script/2017/header.js
@@ -104,3 +104,24 @@ function hasScrolled() {
 
     lastScrollTop = st;
 }
+
+
+
+//
+// not so much a header function, as the action to perform on links clicked in the header
+//
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
