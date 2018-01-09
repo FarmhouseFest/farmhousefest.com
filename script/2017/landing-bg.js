647876ab32c555e6c2833f25aebc87024f5fe79d
Dave Shea <dave@mezzoblue.com>
2018-01-08 20:55:10 -0800

    2018 landing page

--- script/2017/landing-bg.js
+++ script/2017/landing-bg.js
@@ -1,4 +1,12 @@
-var arr = ['pour','field','glass'];
+var arr = [
+	'band',
+	'field',
+	'field2',
+	'gear',
+	'glass',
+	'glass2',
+	'pour2'
+];
 var idx = Math.floor(Math.random() * arr.length);
 
-$("body").addClass("photo-" + arr[idx]);
+$("div.background").addClass("photo-" + arr[idx]);
