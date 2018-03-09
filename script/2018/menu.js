9ca5468b9ed90106759215eeb980165246884b27
Dave Shea <dave@mezzoblue.com>
2018-03-08 23:44:57 -0800

    Mobile menu for 2018

---
+++ script/2018/menu.js
@@ -0,0 +1,96 @@
+// give the menu a close control
+
+$("ul.menu").prepend("<a href='#' class='close-control'>&#x2715;</a>");
+
+
+// click handler for the close control
+
+$("a.close-control").click(function() {
+
+	$("ul.menu").toggleClass("is-active");
+	return false;
+
+});
+
+
+
+// click handler for the menu control
+
+$("a.menu-icon").click(function() {
+
+	$("ul.menu").toggleClass("is-active");
+	return false;
+
+});
+
+
+
+// close menu when option is selected
+
+$("li.nav-item a").click(function() {
+
+	$("ul.menu").toggleClass("is-active");
+	return false;
+
+});
+
+
+
+
+
+
+// adjust mobile menu display based on breakpoints
+
+var $wind = $(window),
+    breakpoints = [320, 480, 640, 800, 960, 1120, 1280],
+    currentBreakpoint = 0;
+    mobile = true;
+
+var mobileFunctions = function() {
+	nav = $("ul.menu").detach();
+	nav.prependTo("body");
+}
+
+var desktopFunctions = function() {
+	nav = $("ul.menu").detach();
+	nav.appendTo(".fhf-header .fhf-nav");
+}
+
+var breakpointHit = function(breakpoint) {
+    mobileCheck();
+}
+
+var mobileCheck = function() {
+
+	var window_w = $wind.width();
+
+	if ( window_w < 960 ) {
+
+		mobileFunctions();
+		mobile = true;
+
+	} else {
+		if ( !mobile ) {
+			return;
+		} else {
+			desktopFunctions();
+			mobile = false;
+		}
+	}
+	
+};
+mobileCheck();
+
+$wind.resize(function() {
+	var i$, len$; window_w = $wind.width();
+    for (i$ = 0, len$ = breakpoints.length; i$ < len$; i$++) {
+        var testPoint = breakpoints[i$];
+        if (window_w < testPoint) {
+             if (testPoint == currentBreakpoint)
+                 break;
+             currentBreakpoint = testPoint;
+             breakpointHit(testPoint);
+             break;
+        }
+    }
+});
